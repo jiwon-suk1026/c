741cc6b9fee5a353af9480d7ffd9b43cdabf73a4
Jaeyoup Jung <skywoduq@cu.ac.kr>
2019-10-15 15:36:54 +0900

    hello

---
+++ Project1/Project1/소스.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	int x = 0;
+	printf(" 4자리 정수 입력 :", x);
+	scanf("%d", &x);
+	printf(" %10d\n", x);
+	printf("%010d %s\n ", x);
+	printf("%+010d %s\n\n", x);
+	printf("%-10d\n", x);
+
+	printf("%10o\n", x);
+	printf("%010o\n", x);
+	printf("%10x\n", x);
+	printf("%0#10x\n", x);
+}
